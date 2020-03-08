496db0941e9b5aadc27d876025b1c1fa995e81f8
Flow Arg <coutureflorence3@gmail.com>
2020-03-08 10:36:33 +0100

    Initial Commit

---
+++ src/main/java/fr/flowarg/sch/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * SPIGOT CONFIGURATION HELPER - 2020 by FlowArg
+ *
+ * @author FlowArg - 2020
+ * Contains a class {@link fr.flowarg.sch.SpigotConfigurationEntry} for help developers to construct easily many configurations.
+ * Contains an interface {@link fr.flowarg.sch.ConfigurationManager} for help developers to construct easily many configuration managers with utils method.
+ * Main package of the lib.
+ */
+package fr.flowarg.sch;
